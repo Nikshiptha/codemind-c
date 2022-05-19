5c7eeed0f1d2ee2d251f72a43d20464d1c69766b
Nikshiptha <97436568+Nikshiptha@users.noreply.github.com>
2022-05-19 06:29:32 -0700

    #include<stdio.h> int main() {     int a,b,t=2,lcm=1;     scanf("%d%d",&a,&b);     while(a>=t&&b>=t)     {         if(a%t==0&&b%t==0)         {             a=a/t;             b=b/t;             lcm=lcm*t;         }         else         {             t++;         }     }     printf("%d",a*b*lcm); }

--- LCM.c
+++ LCM.c
@@ -1,15 +1,20 @@
 #include<stdio.h>
 int main()
 {
-  int a,b;
-  scanf("%d%d",&a,&b);
-  int lcm=(a>b)?a:b;
-  while(1)
-  {
-    if(lcm%a==0&&lcm%b==0)
-    break;
-    lcm++;
-  }
-  printf("%d",lcm);
-  return 0;
+    int a,b,t=2,lcm=1;
+    scanf("%d%d",&a,&b);
+    while(a>=t&&b>=t)
+    {
+        if(a%t==0&&b%t==0)
+        {
+            a=a/t; 
+            b=b/t;
+            lcm=lcm*t;
+        }
+        else
+        {
+            t++;
+        }
+    }
+    printf("%d",a*b*lcm);
 }
