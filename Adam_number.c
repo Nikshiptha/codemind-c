31bfe475961cebbfc127db97b8b0c006ea8e8130
Nikshiptha <97436568+Nikshiptha@users.noreply.github.com>
2022-05-20 04:15:55 -0700

    #include<stdio.h> int main() {     int n,sq,rev=0,qs,d,s,v=0;     scanf("%d",&n);     sq=n*n;     while(n!=0)     {         d=n%10;         n=n/10;         rev=rev*10+d;
        }
        qs=rev*rev;
        while(qs!=0)
        {
            s=qs%10;
            qs=qs/10;
            v=v*10+s;
            }
            if(sq==v)
            {
                   printf("True");
            }
            else
            {
                printf("False");
            }
    }

--- Adam_number.c
+++ Adam_number.c
@@ -1,29 +1,29 @@
 #include<stdio.h>
 int main()
 {
-    int num,r,k=0,s=1,u,v=0,t=1;
-    scanf("%d",&num);
-    s=num*num;
-    while(num!=0)
+    int n,sq,rev=0,qs,d,s,v=0;
+    scanf("%d",&n);
+    sq=n*n;
+    while(n!=0)
     {
-        r=num%10;
-        k=k*10+r;
-        num=num/10;
-    }
-    t=k*k;
-    while(t!=0)
-    {
-        u=t%10;
-        v=v*10+u;
-        t=t/10;
+        d=n%10;
+        n=n/10;
+        rev=rev*10+d;
         
     }
-    if(s==v)
+    qs=rev*rev;
+    while(qs!=0)
     {
-        printf("True");
-    }
-    else
-    {
-        printf("False");
-    }
+        s=qs%10;
+        qs=qs/10;
+        v=v*10+s;
+        }
+        if(sq==v)
+        {
+               printf("True");
+        }
+        else
+        {
+            printf("False");
+        }
 }
